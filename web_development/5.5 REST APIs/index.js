77503fcc265afa440d2968595eac9a20a47876f9
Michael Gnaho <gnahomichael@gmail.com>
2024-05-27 19:49:25 +0100

    update

--- web_development/5.5 REST APIs/index.js
+++ web_development/5.5 REST APIs/index.js
@@ -12,7 +12,7 @@ const API_URL = "https://secrets-api.appbrewery.com";
 // https://secrets-api.appbrewery.com/
 
 //TODO 1: Add your own bearer token from the previous lesson.
-const yourBearerToken = "";
+const yourBearerToken = "1c03f7a6-5766-49d2-ba3b-c5ef8d0060eb";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
